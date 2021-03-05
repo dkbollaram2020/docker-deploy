e890247ab7f101547f091ac4444ce139db7f07cb
Dileep <dkbollaram2020@gmail.com>
2021-03-05 21:41:27 +0000

    Add files via upload

--- server.py
+++ server.py
@@ -5,17 +5,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def get_home():
-    return jsonify({"message": "server works"})
+    return jsonify({"message": "server works..."})
 
-@app.route('/predict_home_price', methods=['POST'])
+@app.route('/predict_home_price')
 def predict_home_price():
     try:
-        postedData = request.get_json()
-        total_sqft = float(postedData['total_sqft'])
-        bhk = int(postedData['bhk'])
-        bath = int(postedData['bath'])
+        bedrooms = int(request.args.get('bedrooms'))
+        bathrooms = int(request.args.get('bathrooms'))
+        square_footage = int(request.args.get('square_footage'))
+        resident_type = request.args.get('resident_type')
         response = jsonify({
-            'estimated_price': util.get_estimated_price(total_sqft,bhk,bath)
+            'estimated_price': util.get_estimated_price(resident_type, bedrooms,bathrooms,square_footage)
         })
         response.headers.add('Access-Control-Allow-Origin', '*')
         return response
@@ -24,7 +24,7 @@ def predict_home_price():
 
 
 if __name__ == "__main__":
-    print("Starting Python Flask Server For Home Price Prediction...")
+    print("Starting server...")
     util.load_saved_artifacts()
     app.run(host='0.0.0.0')
     # app.run(debug=True)
--- util.py
+++ util.py
@@ -2,29 +2,37 @@ import pickle
 import json
 import numpy as np
 
+__data_columns = None
 __model = None
 
-def get_estimated_price(sqft,bhk,bath):
+def get_estimated_price(resident_type,bedrooms,bathrooms,square_footage):
+    try:
+        loc_index = __data_columns.index(resident_type.lower())
+    except:
+        loc_index = -1
 
-    x = np.zeros(3)
-    x[0] = sqft
-    x[1] = bath
-    x[2] = bhk
+    x = np.zeros(len(__data_columns))
+    x[0] = bedrooms
+    x[1] = bathrooms
+    x[2] = square_footage
+    if loc_index>=0:
+        x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
+    return round(__model.predict([x])[0] * 1000, 2)
 
 
 def load_saved_artifacts():
-    print("loading saved artifacts...start")
+    print("loading saved artifacts...")
+    global  __data_columns
+    with open("./artifacts/columns.json", "r") as f:
+        __data_columns = json.load(f)["data_columns"]
 
     global __model
     if __model is None:
-        with open('./artifacts/home_prices_model.pickle', 'rb') as f:
+        with open('./artifacts/rental_comp_arizona_model.pickle', 'rb') as f:
             __model = pickle.load(f)
-    print("loading saved artifacts...done")
+    print("Done loading saved artifacts...")
 
 
 if __name__ == '__main__':
     load_saved_artifacts()
-    print(get_estimated_price(1000, 3, 3))
-    print(get_estimated_price(1000, 2, 2))
